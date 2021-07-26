001998bf4bb0a8659f0b78d1ce1539d2509421bf
Luke Poirrier <lukepoirrier5@gmail.com>
2021-07-26 18:38:51 -0400

    - Add Object "times" to store times for each hour row - Add Object "plans" to store descriptions for each hour row - Add fn to display each description store in times - Add fn to read localStorage and parse the stored data - Update plans if localStorage plans are available

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,68 @@
 var hours = $(".hour");
+// The time of day in 24H time for each row
+var times = {
+  0: 9,
+  1: 10,
+  2: 11,
+  3: 12,
+  4: 13,
+  5: 14,
+  6: 15,
+  7: 16,
+  8: 17,
+};
+
+// Placeholder variable for local storage events
+var plans = {
+  0: "Test",
+  1: "Test",
+  2: "1",
+  3: "",
+  4: "2",
+  5: "3",
+  6: "",
+  7: "",
+  8: "",
+};
+
+// Display descriptions stored in the plans object
+function displayDesc() {
+  for (i = 0; i < 9; i++) {
+    $("#" + i).val(plans[i]);
+  }
+}
+
+function readDesc(){
+  var storedPlans = JSON.parse(localStorage.getItem("storedPlans"));
+  if(storedPlans){
+    plans = storedPlans;
+  }
+}
+
+function storeDesc(){
+
+}
+
+displayDesc();
 $(".hour").each(function (index, element) {
-      console.log(index);
-      
+  console.log(index);
+  // Get current time from moment
+  // var momentHour = moment().format("HH");
+  var momentHour = "11";
+  // Get ID of each hour rows textboxes
+  var rowHour = $(this).siblings(".description").children().attr("id");
+  console.log(times[rowHour]);
+  console.log(rowHour);
+  // If row hour < momentHour - past. If row hour === moment() - present. If hour row > moment() - future
+  if (times[rowHour] < momentHour) {
+    $(this).siblings(".description").addClass("past");
+  } else if (times[rowHour] == momentHour) {
+    $(this).siblings(".description").addClass("present");
+  } else if (times[rowHour] > momentHour) {
+    $(this).siblings(".description").addClass("future");
+  }
 });
+
+function init(){
+  
+}
